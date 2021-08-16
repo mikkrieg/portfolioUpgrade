2b9f5f31de990986bf51250926327550f7779d94
Mikey Kriegel <mikkrieg@gmail.com>
2021-08-15 20:45:24 -0700

    Create necessary components and set up react router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import React from 'react';
+import Nav from './components/Nav';
+import Home from './components/Home';
+import About from './components/About';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <h1>Hello</h1>
+    <Router>
+      <Nav/>
+      <Switch>
+        <Route path='/' exact component={Home} />
+        <Route path='/about' component={About} />
+      </Switch>
+    </Router>
   );
 }
 
