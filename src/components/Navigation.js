ec2c933c68086ba641904834cc9fdf9eb0dd4c93
Mikey Kriegel <mikkrieg@gmail.com>
2021-09-06 19:31:53 -0700

    Begin building navbar, 80% complete

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,30 +2,26 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
+import './../sass/components/navigation.scss';
 import { Link } from 'react-router-dom';
 
 const Navigation = () => {
   return(
-    <Navbar bg='light' expand='lg'>
+    <>
+    <Navbar bg='dark' variant='dark' fixed="top">
       <Container>
-        <Link to="/">
-          <Navbar.Brand>&lt; / &gt;</Navbar.Brand>
-        </Link>
-        <Navbar.Toggle aria-controls='basic-navbar-nav' />
-        <Navbar.Collapse id='basic-navbar-nav' />
+      <Navbar.Brand className='brand' href='/'>&lt;/&gt;</Navbar.Brand>
+      <div className='justify-content-end'>
         <Nav className='me-auto'>
-          <Link to="/">
-            <Nav.Item>Home</Nav.Item>
-          </Link>
-          <Link to="/about">
-            <Nav.Item>About</Nav.Item>
-          </Link>
-          <Nav.Item>Get in Touch!</Nav.Item>
+          <Nav.Link href='/'>Home</Nav.Link>
+          <Nav.Link href='/about'>About</Nav.Link>
+          <Nav.Link>Get in Touch!</Nav.Link>
         </Nav>
+      </div>
       </Container>
     </Navbar>
+    </>
   )
 }
-
 
 export default Navigation;
