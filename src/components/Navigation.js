d08eda4b3e35bb917cd015abc8b7be7e6bac8d5b
Mikey Kriegel <mikkrieg@gmail.com>
2021-10-11 14:48:40 -0700

    Finish placement of navbar and pick home icon to be used

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,21 +3,20 @@ import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import './../sass/components/navigation.scss';
-import { Link } from 'react-router-dom';
+import img from './../img/icons8-stingray-50.png';
 
 const Navigation = () => {
   return(
     <>
-    <Navbar bg='dark' variant='dark' fixed="top">
+    <Navbar collapseOnSelect fixed="top">
       <Container>
-      <Navbar.Brand className='brand' href='/'>&lt;/&gt;</Navbar.Brand>
-      <div className='justify-content-end'>
-        <Nav className='me-auto'>
-          <Nav.Link href='/'>Home</Nav.Link>
-          <Nav.Link href='/about'>About</Nav.Link>
-          <Nav.Link>Get in Touch!</Nav.Link>
-        </Nav>
-      </div>
+        <Navbar.Brand className='brand mt-3' href='/'><img src={img} alt='stingray icon'/></Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Nav className='align-content-end mt-3'>
+            <Nav.Link href='/'>Home</Nav.Link>
+            <Nav.Link href='/about'>About</Nav.Link>
+            <Nav.Link>Get in Touch!</Nav.Link>
+          </Nav>
       </Container>
     </Navbar>
     </>
@@ -25,3 +24,29 @@ const Navigation = () => {
 }
 
 export default Navigation;
+
+// {/* <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+//   <Container>
+//   <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+//   <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+//   <Navbar.Collapse id="responsive-navbar-nav">
+//     <Nav className="me-auto">
+//       <Nav.Link href="#features">Features</Nav.Link>
+//       <Nav.Link href="#pricing">Pricing</Nav.Link>
+//       <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+//         <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+//         <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+//         <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+//         <NavDropdown.Divider />
+//         <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+//       </NavDropdown>
+//     </Nav>
+//     <Nav>
+//       <Nav.Link href="#deets">More deets</Nav.Link>
+//       <Nav.Link eventKey={2} href="#memes">
+//         Dank memes
+//       </Nav.Link>
+//     </Nav>
+//   </Navbar.Collapse>
+//   </Container>
+// </Navbar> */}
