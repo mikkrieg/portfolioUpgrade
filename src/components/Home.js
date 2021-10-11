2bf77fe20a25cdc188af14855112c9389eff4c9b
Mikey Kriegel <mikkrieg@gmail.com>
2021-10-11 15:25:09 -0700

    Add content to home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
 import './../sass/components/home.scss';
 
 const Home = () => {
   return(
-    <h1>Home</h1>
+    <>
+      <body>
+        <h1>Michael Kriegel</h1>
+        <h5>Full Stack Developer</h5>
+        <h4>Scroll down to check out my projects or click here to learn more about me!</h4>
+        <div className='text-center mt-4'>
+          <Button variant='outline-dark' size='lg'>Learn More</Button>
+        </div>
+      </body>
+    </>
   )
 }
 
