094608e9f82b1816b65480d49987ce4fe112d3de
Mikey Kriegel <mikkrieg@gmail.com>
2021-10-11 17:09:00 -0700

    Add footer, add content and link up button

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './../sass/components/footer.scss';
+
+const Footer = () => {
+  return(
+    <footer>&copy;2021 Michael Kriegel</footer>
+  )
+}
+
+
+export default Footer;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
+import Nav from 'react-bootstrap/Nav';
 import './../sass/components/home.scss';
 
 const Home = () => {
@@ -10,7 +11,7 @@ const Home = () => {
         <h5>Full Stack Developer</h5>
         <h4>Scroll down to check out my projects or click here to learn more about me!</h4>
         <div className='text-center mt-4'>
-          <Button variant='outline-dark' size='lg'>Learn More</Button>
+          <Nav.Link href='/about'><Button variant='outline-dark' size='lg'>Learn More</Button></Nav.Link>
         </div>
       </body>
     </>
