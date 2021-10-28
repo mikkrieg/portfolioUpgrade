b754648d86815c0004b4fcf6035373b1e97f5fd5
Mikey Kriegel <mikkrieg@gmail.com>
2021-10-27 21:48:35 -0700

    Rebuild home page with MUI, add MUI

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,51 +1,75 @@
-import React, {useState} from 'react';
-import Button from 'react-bootstrap/Button';
-import Nav from 'react-bootstrap/Nav';
-import Container from 'react-bootstrap/Container';
-import './../sass/components/home.scss';
+import React, { useState } from 'react';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import Grid from '@mui/material/Grid';
+import Button from '@mui/material/Button';
+import { Link } from 'react-router-dom';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import '../sass/components/home.scss';
 import JS from './../img/icons8-javascript-50.png'; 
 import C from './../img/icons8-c-sharp-logo-50.png';
 import R from './../img/icons8-react-native-50.png';
 import Redux from './../img/icons8-redux-50.png';
-import Templix from './projects/Templix';
+// import Templix from './projects/Templix';
 import img from './../img/icons8-down-arrow-64.png';
 
-const Home = () => {
-  const [showP, setShowP] = useState(false);
+const theme = createTheme({
+  palette: {
+    outlinedButtons : {
+      main: '#000',
+      constrastText: '#fff'
+    }
+  }
+  
+})
 
-  const showParagraph = () => {
-    if(window.innerWidth <= 960) {
-      setShowP(false);
-    } else {
-      setShowP(true);
+const Home = () => {
+  const [hideArrow, setHideArrow] = useState(false);
+
+  const removeArrow = () => {
+    if(window.scrollY > 100) {
+      setHideArrow(true);
     }
   }
 
-  window.addEventListener('resize', showParagraph);
+  window.addEventListener('scroll', removeArrow);
   return(
     <>
-      <Container>
-        <div className="home-div">
-          <h1>Michael Kriegel</h1>
-          <h5>Full Stack Developer</h5>
-          <div className='text-center'>
+      <Container maxWidth='sm'>
+        <Grid container>
+          <Grid item xs={12} mt={15}>
+            <Typography variant='h2' align='center'>
+              Michael Kriegel
+            </Typography>
+          </Grid>
+          <Grid item xs={12}>
+            <Typography variant='h5' align='center'>
+              Full Stack Developer
+            </Typography>
+          </Grid>
+          <Grid item xs={8} md={12} sx={{ marginLeft: 9 }}>
             <img src={JS} alt='JavaScript icon'/>
             <img src={C} alt='C sharp icon'/>
             <img src={R} alt='React icon'/>
             <img src={Redux} alt='Redux icon'/>
-          </div>
-          <h4 className='desktop-p'>Scroll down to check out my projects or click here to learn more about me!</h4>
-          <div className='text-center mt-4'>
-            <Nav.Link href='/about'><Button variant='outline-dark' size='lg'>Learn More</Button></Nav.Link>
-          </div>
-        </div>
-        <img src={img} alt='down arrow' className='scroll-arrow'/>
+          </Grid>
+          <Grid item>
+            <Typography variant='h4' align='center' sx={{ display: {xs: 'none', sm: 'none', md: 'block'}}}>
+              Scroll down to check out my projects or click here to learn more about me!
+            </Typography>
+          </Grid>
+          <Grid item xs={12} align='center' mt={2}>
+            <ThemeProvider theme={theme}>
+              <Button component={Link} color='outlinedButtons' to='/about' variant='outlined' sx={{ color: '#000'}}>Learn More!</Button>
+            </ThemeProvider>
+          </Grid>
+          <Grid item xs={1} mt={10}>
+            <img src={img} alt='down arrow' className={hideArrow ? 'hide-arrow' : 'scroll-arrow'}/>
+          </Grid>
+        </Grid>
+        <Grid item xs={12}>
+        </Grid>
       </Container>
-      <div className="first-block">
-        <div className="project-block">
-          <Templix/>
-        </div>
-      </div>
     </>
   )
 }
